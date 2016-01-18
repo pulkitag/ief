77e336189ef32217e62145354ff3d2ca1316d519
pulkitag <pulkitag@berkeley.edu>
2016-01-18 05:10:32 -0800

    README Updated

---
+++ install_requirements.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+sudo pip install easydict
+sudo apt-get install python-h5py libfreetype6-dev
+sudo pip install python-dateutil
+sudo pip install -U matplotlib
--- setup.sh
+++ setup.sh
@@ -6,10 +6,10 @@
 # --------------------------------------------------------
 
 #!/bin/bash
-SRC_URL='www.cs.berkeley.edu:~/pulkitag/ief/'
+SRC_URL='http://www.cs.berkeley.edu:~/pulkitag/ief/'
 #Download the models
 wget -l 0 ${SRC_URL}/models.tar ./
 tar -xf models.tar
 rm models.tar
-#Download the mpii data
-
+#Get pycaffe utils
+git clone https://github.com/pulkitag/pycaffe-utils.git src/pycaffe_utils
